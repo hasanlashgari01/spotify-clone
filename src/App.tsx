60a25602e83e37cad39f58159469422496e21b3d
erfanbyt028 <erfan.bayaat82@gmail.com>
2025-08-27 16:09:16 +0330

    change home route to /

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-<<<<<<< HEAD
-import ReactQueryProvider from "./providers/react-query-provider";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-=======
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import Home from "./pages/Home";
 import ReactQueryProvider from "./providers/react-query-provider";
->>>>>>> header-Erfan
 
 function App() {
   return (
     <ReactQueryProvider>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
-
-          <Route path="/" element={<Navigate to="/home" replace />} />
-          
-
-          <Route path="/home" element={<HomePage />} />
-          
-
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          
-
-          <Route path="*" element={<div>404 - Page Not Found</div>} />
-=======
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
->>>>>>> header-Erfan
         </Routes>
       </BrowserRouter>
     </ReactQueryProvider>
