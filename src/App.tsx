66c185cb9f44d0b5061b29cff642df56cbc42e0d
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-08-29 20:17:42 +0330

    fix: bug in get user

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,9 @@ import RegisterPage from './pages/RegisterPage';
 import ReactQueryProvider from './providers/react-query-provider';
 import Profile from './pages/Profile';
 import HomePage from './pages/HomePage';
-// import { ProtectedRoute } from './components/ProtectedRoute';
+import { ProtectedRoute } from './components/Protect Route/ProtectedRoute';
 
 function App() {
-  localStorage.setItem(
-    'accessToken',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjQsImVtYWlsIjoibWFoZGlAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpc0JhbiI6ZmFsc2UsInN0YXR1cyI6InB1YmxpYyIsImlhdCI6MTc1NjEyNzIwOSwiZXhwIjoxNzU2NzMyMDA5fQ.e3SI5Mqy08lNq3SWD-0Z1Xa6lqB9SlkcWTh_nAz0Els'
-  );
   return (
     <ReactQueryProvider>
       <BrowserRouter>
@@ -18,15 +14,14 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<Profile />} />
-          {/* <Route
-            path="/Profile"
+          <Route
+            path="/profile"
             element={
               <ProtectedRoute>
-         
+                <Profile />
               </ProtectedRoute>
             }
-          /> */}
+          />
         </Routes>
       </BrowserRouter>
     </ReactQueryProvider>
