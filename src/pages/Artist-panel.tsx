15c0273c46a7265a4cb1c2a33a2b366b7cb93b36
amirmahdi148 <amir9580mahdi110@gmail.com>
2025-10-23 16:48:10 +0000

    ArtistPanel

---
+++ src/pages/Artist-panel.tsx
@@ -0,0 +1,17 @@
+
+const ArtistPanel = () => {
+  return (
+    <>
+      <div className="flex ">
+        <div className=" w-[20%] h-[100vh] flex flex-col items-center justify-evenly bg-amber-200">
+          {/* Sidebar component here */}
+
+        </div>
+        <div className="w-[80%]">
+
+        </div>
+      </div>
+    </>
+  );
+}
+export default ArtistPanel;
